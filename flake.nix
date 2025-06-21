d5b6272e7ce80e5448b6789f0b7d6dea653c8e8e
Pierrelefort <pierre.lefort@epita.fr>
2025-06-21 14:50:26 +0200

    feat: use of modules for nixosConfigurations

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "A very basic flake";
+  description = "Computers's nix configuration";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
@@ -7,9 +7,17 @@
 
   outputs = { self, nixpkgs }: {
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
+    nixosConfigurations = {
+      server = nixpkgs.lib.nixosSystem {
+        modules = [
+          ./nixos/server/configuration.nix
+        ];
+      };
+      framework = nixpkgs.lib.nixosSystem {
+        modules = [
+          ./nixos/framework/configuration.nix
+        ];
+      };
+    };
   };
 }
